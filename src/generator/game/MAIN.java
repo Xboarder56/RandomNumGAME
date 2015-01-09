939e5365781dff7bce9e4237457432de1b35ad95
xboarder56 <garrettbeasley@icloud.com>
2015-01-08 19:55:13 -0800

    Finalized build

--- src/generator/game/MAIN.java
+++ src/generator/game/MAIN.java
@@ -20,7 +20,11 @@ package generator.game;
 import java.util.Scanner;
 import java.awt.Color;
 
-/**This is the MAIN Class for the game to be launched from
+/**This is the MAIN Class for the game to be launched from.
+ * The game consists of 4 options for the user to select
+ * Array, Coin, Color, Exit. 
+ * Once the option is selected the user then completes the game
+ * and will have a option to enter the game once again. 
  */
 public class MAIN 
 {
@@ -51,25 +55,48 @@ public class MAIN
 		System.out.println("4: Exit!");
 	}
 	
+	/**
+	 * Input One (when the user hits 1)
+	 * @description When 1 is pressed the program will enter
+	 * into the inputOne method and calculate the array based
+	 * off the three input values passed in by the user. min/max/array length
+	 * then will output them to the console.
+	 */
 	public static void inputOne()
 	{
+		/*Console Input taken from the user for the custom arrays*/
 		Scanner console = new Scanner(System.in);
 		System.out.println("How many elements do you want in your array?");
 		int arrayElements = console.nextInt();
-		console.nextLine();
+		console.nextLine(); //clears the prompt (to skip enter key)
+		
+		/*Console Input taken from the user for the custom arrays*/
 		System.out.println("Min value?");
 		int arrayMinValue= console.nextInt();
-		console.nextLine();
+		console.nextLine(); //clears the prompt (to skip enter key)
+		
+		/*Console Input taken from the user for the custom arrays*/
 		System.out.println("Max value?");
 		int arrayMaxValue = console.nextInt();
-		console.nextLine();
+		console.nextLine(); //clears the prompt (to skip enter key)
 		
+		/*Create a new Array called Array with
+		 * the max array length based of user input into arrayElements*/
 		Array = new int[arrayElements];
 		
+		/*Loop the array for (arrayElements) time*/
 		for(int i=0; i<arrayElements; i++)
 		{
+			/*Array with values randomly based on the users min/max values*/
 			Array[i] =  ((int) (Math.random( )*(arrayMaxValue))+arrayMinValue);
+			
+			/*Print the random array values out for the user to see*/
 			System.out.print(Array[i]+" ");
+			
+			/*Used for closing the console
+			 * If anyone sees this
+			 * This is a bad method to use!
+			 */
 			loopCounter++;
 		}
 		
@@ -78,39 +105,65 @@ public class MAIN
 		 * or we get scanner util error*/
 		if((loopCounter == arrayElements+1))
 		{
+			/*close the console*/
 			console.close();
 		}
 	}
 	
+	/**
+	 * Input Two (when the user hits 2)
+	 * @description When 2 is pressed the program will enter
+	 * into the inputTow method and calculate the flip of a coin
+	 * off the input value the user enters in for how many times to flip
+	 */
 	public static void inputTwo()
 	{
+		/*Console Input taken from the user for the amount of flips*/
 		Scanner console = new Scanner(System.in);
 		System.out.println("How many coins do you want to flip? ");
 		int coinAmount = console.nextInt();
-		console.nextLine();
+		console.nextLine(); //clears the prompt (to skip enter key)
 		
+		/*Loop for the amount of flips the user specifies*/
 		for(int i=0; i<=coinAmount; i++)
 		{
+			/*Coin only has 2 sides heads(0) tails(1)*/
 			int randomValue = (int) (Math.random( )*(2));
 			
+			/*Check to see if coin is heads aka 0 */
 			if(randomValue == 0)
 			{
+				/*Increases the heads number*/
 				heads++;
 			}
+			
+			/*Check to see if coin is tails aka 1 */
 			else if(randomValue == 1)
 			{
+				/*Increases the tails number*/
 				tails++;
 			}
 		}
 		
+		/*Prints to console what was generated*/
 		System.out.println("You flipped "+ heads + " heads, " + tails +" tails");
+		
+		/*closes the scanner for the user*/
 		console.close();
 		
 	}
 	
+	/**
+	 * Input Three (when the user hits 3)
+	 * @description When 3 is pressed the program will enter and
+	 * start to calculate a random color and build the color
+	 * under the variable named rand. 
+	 */
 	public static void inputThree()
 	{
+		/*Let the user know that we are generating a new color*/
 		System.out.println("Generating a random color...");
+		
 		/*Color Randomizer to make it flash using the math.random class as the Random generator fails to work*/
 		int RED = (int) (Math.random( )*256);
 		int GREEN = (int)(Math.random( )*256);
@@ -118,12 +171,23 @@ public class MAIN
 		
 		/*Makes new color*/
 		Color rand = new Color(RED,GREEN,BLUE);
+		
+		/*Print new color*/
 		System.out.println(rand);
 	}
 	
+	/**
+	 * Input 4 (when the user hits 4)
+	 * @description When 4 is pressed the program will enter
+	 * into the inputFour method and tell the user goodbye
+	 * then the system will exit the java machine.
+	 */
 	public static void inputFour()
 	{
+		/*Goodbye to the user and thank them*/
 		System.out.println("Thanks for playing!!! :)");
+		
+		/*Close the java machine*/
 		System.exit(0);
 	}
 	
@@ -134,32 +198,48 @@ public class MAIN
 	 */
 	public static void inputReader()
 	{
+		/*Tell the user to pick a selection between 1-4*/
 		Scanner console = new Scanner(System.in);
 		System.out.println("Please enter a selection: ");
-		int selection = console.nextInt();
+		int selection = console.nextInt(); //clears the prompt (to skip enter key)
 		console.nextLine();
+		
+		/*If user enters 1 enter this section*/
 		if(selection == 1)
 		{
+			/*Go to 1 then loop the code after*/
 			inputOne();
 			inputReader();
 		}
+		
+		/*If user enters 2 enter this section*/
 		else if(selection == 2)
 		{
+			/*Go to 2 then loop the code after*/
 			inputTwo();
 			inputReader();
 		}
+		
+		/*If user enters 3 enter this section*/
 		else if(selection == 3)
 		{
+			/*Go to 3 then loop the code after*/
 			inputThree();
 			inputReader();
 		}
+		
+		/*If user enters 4 enter this section*/
 		else if(selection == 4)
 		{
+			/*Go to 4 then loop the code after*/
 			inputFour();
 			inputReader();
 		}
+		
+		/*Anything else other then 1-4 will go down here*/ 
 		else
 		{
+			/*Tell the user to please enter a number between 1-4 and loop the code*/
 			System.out.println("Not a valid selection please enter a number between 1-4");
 			inputReader();
 		}
